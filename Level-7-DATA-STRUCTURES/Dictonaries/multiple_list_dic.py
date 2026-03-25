42cfef2c5fc54db9ffef0b3b456e5d0c3e9e3e2d
KuruvaBasavaraja <kuruva.basavaraja09@gmail.com>
2026-03-25 11:25:53 +0530

    update dict files

--- Level-7-DATA-STRUCTURES/Dictonaries/multiple_list_dic.py
+++ Level-7-DATA-STRUCTURES/Dictonaries/multiple_list_dic.py
@@ -32,3 +32,32 @@ def search_movies_by_language(language):
 print(search_movies_by_language("English"))
 
 #output: ['Inception', 'Interstellar']
+
+#in with LIST
+print("Inception" in ["Inception", "Bahubali"]) #True
+
+#in with DICTIONARY
+if "name" in movies[0]:
+    print("Key exists")
+
+
+#in with .values()
+if "English" in movies[0].values():
+    print("Value exists")
+
+#in with LOOP 
+for movie in movies:
+    if "English" in movie.values():
+        print(movie["name"])
+
+# Reordering Dictionary Keys by Inserting a New Key at Specific Position
+# ## Adding 'rating' key after 'director' by converting dictionary to ordered list
+items = list(movies[0].items())
+
+items.insert(2, ("rating", 8.8))  # position after director
+
+movies[0] = dict(items)
+
+print(movies[0])
+
+#Since dictionaries don’t support positional insertion, so convert it to a list of items, insert at the required index, and reconstruct the dictionary.
